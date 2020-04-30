9908e784353426d61d99770ddeb07b0299ef30e5
Alessio Iantosca <alessio.iantosca@gmail.com>
2020-04-30 11:48:31 +0200

    new project initialized by mvn

---
+++ src/main/java/ch/bbw/aimf/FotosharerApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.aimf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FotosharerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FotosharerApplication.class, args);
+	}
+
+}
